348765b138653fe7afe77c3144703ad8a202326f
zzinx58 <q1095568627@163.com>
2022-10-23 11:25:57 +0800

    setup: about tsConfig and viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from "vite-plugin-windicss"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import {ElementPlusResolver} from "unplugin-vue-components/resolvers"
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    WindiCSS(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dirs:["scr/components", "src/views"],
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve("./src"),
+      },
+    ]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use as *;
+        `
+      }
+    }
+  }
 })
